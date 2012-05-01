ee91c97dd2fcbe6cef4c46937a3fde0e547ef692
Jakob Gruber <jakob.gruber@gmail.com>
2012-05-01 08:48:58 +0200

    Add basic logging to frontend

--- src/client/gui/loginwindow.cpp
+++ src/client/gui/loginwindow.cpp
@@ -1,32 +1,49 @@
 #include "loginwindow.h"
-#include "sessionmanager.h"
+
 #include <QtDeclarative>
+
+#include "sessionmanager.h"
+#include "QsLog.h"
+
 namespace sdcc
 {
+
 LoginWindow::LoginWindow(QWidget *parent) :
     QDeclarativeView(QUrl("qrc:/qml/Login.qml"), parent)
 {
+    QLOG_TRACE() << "SDC client starting up...";
+
     setWindowTitle("SDC");
     setResizeMode(QDeclarativeView::SizeRootObjectToView);
+
     rootContext()->setContextProperty("context", this);
     QObject * root = dynamic_cast<QObject*>(rootObject());
+
     connect(root, SIGNAL(serverTest(QString)), SLOT(serverTest(QString)));
-    connect(SessionManager::getInstance(), SIGNAL(testConnectionCompleted(bool,QString)), SLOT(serverTestCallback(bool,QString)));
+    connect(SessionManager::getInstance(),
+            SIGNAL(testConnectionCompleted(bool,QString)),
+            SLOT(serverTestCallback(bool,QString)));
 }
 
 QString LoginWindow::fileDialog(const QString &name, const QString &dir, const QString &types)
 {
     return QFileDialog::getOpenFileName(this, name, dir, types);
 }
+
 void LoginWindow::serverTest(const QString & uri)
 {
+    QLOG_TRACE() << "Testing connection to " << uri;
     SessionManager::getInstance()->testConnection(uri);
 }
+
 void LoginWindow::serverTestCallback(bool success, const QString &message)
 {
+    QLOG_TRACE() << "Received reply to connection test: success " << success
+                 << ", message " << message;
     QMetaObject::invokeMethod(rootObject(),
                               "serverTestCallback",
                               Q_ARG(QVariant, QVariant(success)),
                               Q_ARG(QVariant, QVariant(message)));
 }
+
 }
--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,5 +1,8 @@
-#include "loginwindow.h"
 #include <QApplication>
+
+#include "loginwindow.h"
+#include "QsLog.h"
+#include "QsLogDest.h"
 
 using namespace sdcc;
 
@@ -7,6 +10,15 @@ int main(int argc, char **argv)
 {
     QApplication a(argc, argv);
     Q_INIT_RESOURCE(client_gui_qml);
+
+    /* Init logger */
+    QsLogging::Logger& logger = QsLogging::Logger::instance();
+    logger.setLoggingLevel(QsLogging::TraceLevel);
+
+    QsLogging::DestinationPtr debugDestination(
+        QsLogging::DestinationFactory::MakeDebugOutputDestination());
+    logger.addDestination(debugDestination.get());
+
     LoginWindow win;
     win.show();
     return a.exec();
