17faa614764beeee134b484b2630475c036b547b
Jakob Gruber <jakob.gruber@gmail.com>
2012-05-15 20:09:22 +0200

    Remove unnecessary clutter from testEchoMessage
    All that setup is already done in init().

--- test/client/backend/chattest.cpp
+++ test/client/backend/chattest.cpp
@@ -163,48 +163,17 @@ void ChatTests::testInvite()
 
 void ChatTests::testEchoMessage()
 {
-    SessionManager *sessionManager = SessionManager::getInstance();
-    QSignalSpy spy(sessionManager,
-                   SIGNAL(loginCompleted(QSharedPointer<Session>,bool,QString)));
+    QSignalSpy spy(chat.data(),
+                   SIGNAL(messageReceived(const User &, const QString &)));
     QVERIFY(spy.isValid());
     QVERIFY(spy.isEmpty());
-
-    User u("fefeb10c@selinux.inso.tuwien.ac.at", WORKING_DIR "public.pem");
-    sessionManager->login("selinux.inso.tuwien.ac.at", WORKING_DIR "ca.crt", u,
-                          "password");
-
-    waitForResult(spy);
-    QCOMPARE(spy.count(), 1);
-    QList<QVariant> arguments = spy.takeFirst();
-    QVERIFY2(arguments.at(1) == true, arguments.at(2).toString().toStdString().c_str());
-
-    session = arguments.at(0).value<QSharedPointer<Session> >();
-
-    QSignalSpy spy2(session.data(), SIGNAL(initChatCompleted(QSharedPointer<Chat>, bool,
-                                           QString)));
-    QVERIFY(spy2.isValid());
-    QVERIFY(spy2.isEmpty());
-
-    session->initChat();
-    waitForResult(spy2);
-
-    QCOMPARE(spy2.count(), 1);
-    QList<QVariant> arguments2 = spy2.takeFirst();
-    QVERIFY2(arguments2.at(1) == true, arguments2.at(2).toString().toStdString().c_str());
-
-    chat = arguments2.at(0).value<QSharedPointer<Chat> >();
-
-    QSignalSpy spy3(chat.data(),
-                    SIGNAL(messageReceived(const User &, const QString &)));
-    QVERIFY(spy3.isValid());
-    QVERIFY(spy3.isEmpty());
 
     QString msg = "send this back to me!";
     chat->send(msg);
 
-    waitForResult(spy3);
+    waitForResult(spy);
 
-    QCOMPARE(spy3.count(), 1);
-    QList<QVariant> arguments3 = spy3.takeFirst();
-    QVERIFY(arguments3.at(1) == msg);
+    QCOMPARE(spy.count(), 1);
+    QList<QVariant> arguments = spy.takeFirst();
+    QVERIFY(arguments.at(1) == msg);
 }
