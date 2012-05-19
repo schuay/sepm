434d1ea15ccbb7e008366cfe3ace017f37435c6b
Jakob Gruber <jakob.gruber@gmail.com>
2012-05-19 14:23:30 +0200

    Form changes to session.h

--- src/server/session.h
+++ src/server/session.h
@@ -1,56 +1,69 @@
 #include "SecureDistributedChat.h"
 #include <Ice/Ice.h>
 
-using namespace sdc;
-
 namespace sdcs
 {
 
-class Session : public SessionI
+class Session : public sdc::SessionI
 {
 
 signals:
-    /* pseudocode?
+    /* pseudocode
       chatCreated(Chatptr, ...);
       chatDestroyed(Chatptr, ...);
     */
 
 public:
     /* destroy self, notify all open chats */
-    void logout(const Ice::Current &); // throw UserHandlingException;
+    void logout(const Ice::Current &) throw(sdc::UserHandlingException);
 
     /* local user: db lookup
      * remote: create interserver, forward call, return. */
-    User retrieveUser(const std::string &userID, const Ice::Current &); // throw UserHandlingException, InterServerException;
+    User retrieveUser(const std::string &userID, const Ice::Current &)
+    throw(sdc::UserHandlingException, sdc::InterServerException);
 
     /* gen ID (protected by sema, uuid@serverurl)
      * create LocalChat, insert in global and own list, add self as participant.
      * idea: server attaches to chatCreated signal.
      * */
-    std::string initChat(const Ice::Current &); // throw SessionException;
+    std::string initChat(const Ice::Current &) throw(sdc::SessionException);
 
     /* notify chat, remove from local list */
-    void leaveChat(const std::string &chatID, const Ice::Current &); // throw SessionException, InterServerException;
+    void leaveChat(const std::string &chatID, const Ice::Current &)
+    throw(sdc::SessionException, sdc::InterServerException);
 
     /* check consistency (chat exists, chat joined)
      * create participant (remote or local), call invite. */
-    void invite(const User &participant, const std::string &chatID,
-                const ByteSeq &sessionKey, const Ice::Current &); // throw UserHandlingException, InterServerException;
+    void invite(const sdc::User &participant, const std::string &chatID,
+                const sdc::ByteSeq &sessionKey, const Ice::Current &)
+    throw(sdc::UserHandlingException, sdc::InterServerException);
 
     /* looks up chat and calls appendMessageFrom. */
-    void sendMessage(const ByteSeq &message, const std::string &chatID, const Ice::Current &); // throw MessageException, InterServerException;
+    void sendMessage(const sdc::ByteSeq &message, const std::string &chatID, const Ice::Current &)
+    throw(sdc::MessageException, sdc::InterServerException);
 
     /* check auth, do db ops, logout (make sure everything is consistent), destroy session */
-    void deleteUser(const User &participant, const Ice::Current &); //throw UserHandlingException;
+    void deleteUser(const User &participant, const Ice::Current &)
+    throw(sdc::UserHandlingException);
 
 
     /* --------------------- */
 
-    void saveLog(const std::string &chatID, long timestamp, const SecureContainer &log, const Ice::Current &); // throw LogException;
-    Loglist retrieveLoglist(const Ice::Current &); //throw LogException;
-    SecureContainer retrieveLog(const std::string &chatID, long timestamp, const Ice::Current &); // throw LogException;
-    void saveContactList(const SecureContainer &contactList, const Ice::Current &); // throw ContactException;
-    SecureContainer retrieveContactList(const Ice::Current &); //  throw ContactException;
+    void saveLog(const std::string &chatID, long timestamp,
+                 const sdc::SecureContainer &log, const Ice::Current &)
+    throw(sdc::LogException);
+
+    sdc::Loglist retrieveLoglist(const Ice::Current &) throw(sdc::LogException);
+
+    sdc::SecureContainer retrieveLog(const std::string &chatID, long timestamp,
+                                     const Ice::Current &)
+    throw(sdc::LogException);
+
+    void saveContactList(const sdc::SecureContainer &contactList, const Ice::Current &)
+    throw(sdc::ContactException);
+
+    sdc::SecureContainer retrieveContactList(const Ice::Current &)
+    throw(sdc::ContactException);
 
 private:
     QMap<QString, Chat> chats;
