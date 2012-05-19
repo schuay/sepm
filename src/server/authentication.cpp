4af153ce1b7f44d0f03dcc48e4286aa268b15f2c
Jakob Gruber <jakob.gruber@gmail.com>
2012-05-19 13:58:35 +0200

    Add throw clauses to Authentication

--- src/server/authentication.cpp
+++ src/server/authentication.cpp
@@ -4,18 +4,21 @@ namespace sdcs
 {
 
 std::string Authentication::echo(const std::string &message, const Ice::Current &)
+throw(sdc::SDCException)
 {
     return message;
 }
 
 sdc::SessionIPrx Authentication::login(const sdc::User &, const std::string &,
                                        const Ice::Identity &, const Ice::Current &)
+throw(sdc::AuthenticationException)
 {
     return NULL;
 }
 
 void Authentication::registerUser(const sdc::User &/*participant*/,
                                   const std::string &/*pwd*/, const Ice::Current &)
+throw(sdc::AuthenticationException)
 {
 
 }
--- src/server/authentication.h
+++ src/server/authentication.h
@@ -6,10 +6,15 @@ namespace sdcs
 class Authentication : public sdc::AuthenticationI
 {
 public:
-    void registerUser(const sdc::User &participant, const std::string &pwd, const Ice::Current &);
+    void registerUser(const sdc::User &participant, const std::string &pwd, const Ice::Current &)
+    throw(sdc::AuthenticationException);
+
     sdc::SessionIPrx login(const sdc::User &participant, const std::string &pwd,
-                           const Ice::Identity &ident, const Ice::Current &);
-    std::string echo(const std::string &message, const Ice::Current &);
+                           const Ice::Identity &ident, const Ice::Current &)
+    throw(sdc::AuthenticationException);
+
+    std::string echo(const std::string &message, const Ice::Current &)
+    throw(sdc::SDCException);
 };
 
 }
