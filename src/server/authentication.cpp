98fdf68ae7bba2c9359b49d674bb6cbc61f30cd9
Jakob Gruber <jakob.gruber@gmail.com>
2012-05-19 13:52:43 +0200

    Minor form fixes in Authentication

--- src/server/authentication.cpp
+++ src/server/authentication.cpp
@@ -1,27 +1,23 @@
 #include "authentication.h"
-#include <string>
 
 namespace sdcs
 {
-Authentication::Authentication()
-{
-}
 
-
-std::string Authentication::echo(const std::string &message, const Ice::Current&)
+std::string Authentication::echo(const std::string &message, const Ice::Current &)
 {
     return message;
 }
 
-SessionIPrx Authentication::login(const User &, const std::string &,
-                                  const Ice::Identity &, const Ice::Current&)
+sdc::SessionIPrx Authentication::login(const sdc::User &, const std::string &,
+                                       const Ice::Identity &, const Ice::Current &)
 {
     return NULL;
 }
 
-void Authentication::registerUser(const User &, const std::string &, const Ice::Current &)
+void Authentication::registerUser(const sdc::User &/*participant*/,
+                                  const std::string &/*pwd*/, const Ice::Current &)
 {
-    return;
+
 }
 
 }
--- src/server/authentication.h
+++ src/server/authentication.h
@@ -1,30 +1,15 @@
 #include "SecureDistributedChat.h"
-#include <QMap>
-
-#include "chat.h"
-// #include "session.h" -> uncomment as soon as plausible
-
-using namespace sdc;
 
 namespace sdcs
 {
 
-class Authentication : public AuthenticationI
+class Authentication : public sdc::AuthenticationI
 {
-
 public:
-    Authentication();
-
-    virtual void registerUser(const User &participant, const std::string &pwd, const Ice::Current &);
-    virtual SessionIPrx login(const User &participant, const std::string &pwd,
-                              const Ice::Identity &ident, const Ice::Current&);
-    virtual std::string echo(const std::string &message, const Ice::Current&);
-
-private:
-    /* Every resource is protected by mutexes. */
-    QMap<QString, Chat> chats;
-    // uncomment as soon as Session is compilable
-    // QMap<QString, Session> sessions;
+    void registerUser(const sdc::User &participant, const std::string &pwd, const Ice::Current &);
+    sdc::SessionIPrx login(const sdc::User &participant, const std::string &pwd,
+                           const Ice::Identity &ident, const Ice::Current &);
+    std::string echo(const std::string &message, const Ice::Current &);
 };
 
 }
