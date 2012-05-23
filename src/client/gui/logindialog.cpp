e031c66286d6a1e66a5780890933c229ab012ce9
chainer1 <e1025882@student.tuwien.ac.at>
2012-05-23 18:50:16 +0200

    Fixed resize Bug. #10441
    The Window now has the correct size after checking the checkbox

--- src/client/gui/logindialog.cpp
+++ src/client/gui/logindialog.cpp
@@ -65,14 +65,14 @@ void LoginDialog::onGroupCheckboxClicked(bool checked)
 
     if (!checked) {
         ui->groupBox_2->setMaximumSize(60, 20);
-        resize(242, 100);
+        int oldSize = this->geometry().width();
+        resize(oldSize, 100);
         adjustSize();
-        resize(242, 100);
+        resize(oldSize, 100);
     } else {
         ui->groupBox_2->setMaximumSize(16777215, 16777215);
-        resize(242, 480);
-        adjustSize();
-        resize(242, 480);
+        int oldSize = this->geometry().width();
+        resize(oldSize, 480);
     }
 }
 
