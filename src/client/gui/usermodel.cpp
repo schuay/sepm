f97c98723e58e8b2aeb7d0821a4d92f74f8a9849
Gabriel Pickl <gabriel@syntax-austria.org>
2012-05-27 22:56:50 +0200

    Moved list conversion to UserModel
    Also added functions with QModelIndex parameters for convenience

--- src/client/gui/usermodel.cpp
+++ src/client/gui/usermodel.cpp
@@ -1,12 +1,24 @@
 #include "usermodel.h"
 #include "sdcHelper.h"
-
+#include "session.h"
+#include <QStringListIterator>
+#include <QsLog.h>
 
 namespace sdcc
 {
 UserModel::UserModel(QObject *parent) :
     QAbstractListModel(parent)
 {
+}
+
+UserModel::UserModel(QSharedPointer<Session> session) :
+    QAbstractListModel(0)
+{
+    d_session = session;
+    connect(d_session.data(),
+            SIGNAL(retrieveUserCompleted(QSharedPointer<const User>, const QObject *, bool, const QString)),
+            this,
+            SLOT(onUserRetrieved(QSharedPointer<const User>, const QObject *, bool, QString)));
 }
 
 int UserModel::rowCount(const QModelIndex &parent) const
@@ -46,6 +58,11 @@ QSharedPointer<const User> UserModel::getUser(int id) const
     return d_users.values().at(id);
 }
 
+QSharedPointer<const User> UserModel::getUser(const QModelIndex &index) const
+{
+    return getUser(index.row());
+}
+
 void UserModel::addUser(QSharedPointer<const User> user)
 {
     if (!d_users.contains(user->getName())) {
@@ -64,6 +81,7 @@ QSharedPointer<const User> UserModel::removeUser(const QString &username)
     if (d_users.contains(username)) {
         QSharedPointer<const User> user = d_users.value(username);
         int row = d_users.keys().indexOf(username);
+        d_users.remove(username);
         emit dataChanged(
             createIndex(row, 0),
             createIndex(rowCount(), 0)
@@ -82,6 +100,41 @@ QSharedPointer<const User> UserModel::removeUser(QSharedPointer<const User> user
 QSharedPointer<const User> UserModel::removeUser(int index)
 {
     return removeUser(d_users.keys().at(index));
+}
+
+QSharedPointer<const User> UserModel::removeUser(const QModelIndex &index)
+{
+    return removeUser(index.row());
+}
+
+void UserModel::fromNameList(const QStringList &userList, QSharedPointer<Session> session)
+{
+    QStringListIterator it(userList);
+    if (session.isNull()) {
+        session = d_session;
+    } else {
+        connect(session.data(),
+                SIGNAL(retrieveUserCompleted(QSharedPointer<const User>, QObject *, bool, const QString)),
+                this,
+                SLOT(onUserRetrieved(QSharedPointer<const User>, QObject *, bool, QString)));
+    }
+    while (it.hasNext()) {
+        QString name = it.next();
+        session->retrieveUser(name, this);
+    }
+}
+
+const QStringList UserModel::toUserList() const
+{
+    return QStringList(d_users.keys());
+}
+
+void UserModel::onUserRetrieved(QSharedPointer<const User> user, const QObject *id, bool success, const QString &)
+{
+    QLOG_TRACE() << __PRETTY_FUNCTION__;
+    if (success && id == this) {
+        addUser(user);
+    }
 }
 
 }
--- src/client/gui/usermodel.h
+++ src/client/gui/usermodel.h
@@ -6,6 +6,7 @@
 #include <QMap>
 #include <QString>
 #include "user.h"
+#include "session.h"
 
 namespace sdcc
 {
@@ -15,22 +16,28 @@ class UserModel : public QAbstractListModel
     Q_OBJECT
 public:
     explicit UserModel(QObject *parent = 0);
+    explicit UserModel(QSharedPointer<Session> session);
     int rowCount(const QModelIndex &parent = QModelIndex()) const;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
 
     QSharedPointer<const User> getUser(const QString &username) const;
     QSharedPointer<const User> getUser(int id) const;
+    QSharedPointer<const User> getUser(const QModelIndex &index) const;
     void addUser(QSharedPointer<const User> user);
     QSharedPointer<const User> removeUser(const QString &username);
     QSharedPointer<const User> removeUser(QSharedPointer<const User> user);
     QSharedPointer<const User> removeUser(int index);
-
+    QSharedPointer<const User> removeUser(const QModelIndex &index);
+    void fromNameList(const QStringList &userList, QSharedPointer<Session> session = QSharedPointer<Session>());
+    const QStringList toUserList() const;
 signals:
 
 public slots:
+    void onUserRetrieved(QSharedPointer<const User> user, const QObject *id, bool success, const QString &);
 
 private:
     QMap<QString, QSharedPointer<const User> > d_users;
+    QSharedPointer<Session> d_session;
 
 };
 }
