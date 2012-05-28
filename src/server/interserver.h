fa0e471217ca8c7226e7af1d8b2f16938b2a40ab
Jakob Gruber <jakob.gruber@gmail.com>
2012-05-29 00:01:48 +0200

    Clarify position of chat and user in InterServer
    Hopefully these comments are correct, they seem to make sense but I'm
    not 100% sure.

--- src/server/interserver.h
+++ src/server/interserver.h
@@ -15,12 +15,9 @@ public:
     sdc::User retrieveUser(const std::string &userID, const Ice::Current &)
     throw(sdc::InterServerException);
 
-
     /**
      * Invite a user on another server to a chat (forward invite).
-     * (interpretation mh: the text above must be wrong and instead
-     * mean: Invite a user to a chat on another server, because
-     * what's written above is done by clientInitChat.)
+     * This is called when the (remote) caller is inviting someone to a callee-local chat.
      */
     void invite(const sdc::User &participant,
                 const std::string &chatID,
@@ -34,6 +31,7 @@ public:
      * on forwarding calls to another server because the chat
      * is on another server or because the user is on another
      * server.
+     * Caller-local user, callee-local chat.
      */
     void sendMessage(const sdc::User &sender,
                      const sdc::ByteSeq &message,
@@ -43,6 +41,7 @@ public:
 
     /**
      * The participant leaves a chat on another server.
+     * Caller-local user, callee-local chat.
      */
     void leaveChat(const sdc::User &participant,
                    const std::string &chatID,
@@ -51,8 +50,7 @@ public:
 
     /**
      * Command a server to relay an initChat request to a client.
-     * (interpretation mh: invite the other server's user to our (local or remote)
-     * chat)
+     * This is called when the (remote) caller is inviting a callee-local user to a chat.
      */
     void clientInitChat(const sdc::User &client,
                         const sdc::StringSeq &users,
@@ -63,6 +61,7 @@ public:
 
     /**
      * Command a server to relay an addChatParticipant request to a client.
+     * Caller-local chat, callee-local user.
      */
     void clientAddChatParticipant(const sdc::User &client,
                                   const sdc::User &participant,
@@ -72,6 +71,7 @@ public:
 
     /**
      * Command a server to relay an removeChatParticipant request to a client.
+     * Caller-local chat, callee-local user.
      */
     void clientRemoveChatParticipant(const sdc::User &client,
                                      const sdc::User &participant,
@@ -81,6 +81,7 @@ public:
 
     /**
      * Command a server to relay an appendMessage request to a client.
+     * Caller-local chat, callee-local user.
      */
     void clientAppendMessageToChat(const sdc::User &client,
                                    const sdc::ByteSeq &message,
